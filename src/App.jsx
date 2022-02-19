b118f485afe236577b86a0f97bd7e853865b22ce
Waflart Pierre <pierre.waflart@gmail.com>
2022-02-20 00:22:05 +0100

    add maintenance

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react'
 import './App.scss'
 import Header from "./components/header/Header";
 import Content from "./components/content/Content";
+import Maintenance from "./components/maintenance/Maintenance";
 
 function App() {
     const [router, setRouter] = useState('about');
@@ -9,12 +10,13 @@ function App() {
 
 
     return (
-        <div className={"app " + theme}>
+        /*<div className={"app " + theme}>
             <div className={"layout"}>
                 <Header router={router} setRouter={setRouter} theme={theme} setTheme={setTheme}/>
                 <Content router={router} theme={theme}/>
             </div>
-        </div>
+        </div>*/
+        <Maintenance/>
     )
 }
 
---
+++ src/components/maintenance/Maintenance.jsx
@@ -0,0 +1,8 @@
+const Maintenance = () => {
+    return(
+        <>
+        <h1>Le site est actuellement en maintenance</h1>
+    </>
+    )
+}
+export default Maintenance
